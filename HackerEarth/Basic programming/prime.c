49bdf4a38181d622bc3685170cabe9d48579452b
bhaskarjha514 <bhaskarjha514>
2019-08-11 12:31:17 +0530

    Basic Programmings

---
+++ HackerEarth/Basic programming/prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+int i, prime, u, n;
+scanf("%d",&u);
+for(n=2; n<u; n++)
+{
+prime = 1;
+for(i=2; i<n; i++)
+{
+if(n%i == 0)
+{
+prime = 0; 
+break;
+}
+}
+if(prime)
+printf("%d ",n);
+}
+}
