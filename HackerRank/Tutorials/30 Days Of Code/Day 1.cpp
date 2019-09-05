118ba4b9c727685ac6e684b3f5ecbc65353c65fb
bhaskarjha514 <bhaskarjha514>
2019-09-05 12:20:22 +0530

    Tutorials Repo Add

---
+++ HackerRank/Tutorials/30 Days Of Code/Day 1.cpp
@@ -0,0 +1,29 @@
+
+
+    
+    // Declare second integer, double, and String variables.
+    int j;
+    double e;
+    string t;
+    cin >> j;
+    cin >> e;
+    string input_string; 
+    cin.ignore();
+    getline(cin, input_string); 
+    
+    cout << i+j <<endl;
+     std::cout << std::fixed << std::setprecision(1) << d+e<<endl;
+    
+    cout << s; 
+    cout << input_string << endl;
+
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
