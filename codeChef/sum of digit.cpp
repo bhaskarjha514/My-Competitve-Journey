1bee5bde06792d695cd241b3e408aa10729dfa4d
bhaskarjha514 <bhaskarjha8285@gmail.com>
2020-08-23 14:08:03 +0530

    sum of digit solved

---
+++ codeChef/sum of digit.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+#include<sstream>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    
+    while(t--){
+        int n; cin>>n;
+        ostringstream str1;
+        str1<<n;
+        string s = str1.str();
+        
+        long long int sum = 0;
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            char x = s[i];
+            int p = x;
+            p = p-48;
+            sum = sum+p;
+        }
+        cout<<sum<<endl;
+    }
+}
