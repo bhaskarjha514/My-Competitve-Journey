ac01303f90b43a091f1440de412086464d6b6c82
Bhaskar Jha <35396885+bhaskarjha514@users.noreply.github.com>
2020-08-27 02:28:03 +0530

    Create reverseInteger.java

---
+++ interview ques/reverseInteger.java
@@ -0,0 +1,20 @@
+// reverse of no
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Enter length of array");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;                     //123
+        int reversed = 0;
+        while(temp>0){
+            int lastDigit = temp%10;
+            reversed = reversed*10+lastDigit;
+            temp/=10;
+            
+        }
+        System.out.print("done "+reversed);
+        
+        
+    }
+}
