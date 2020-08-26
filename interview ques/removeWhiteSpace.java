a3aab99341cf52bf985229c5a9395a2f83a2e840
Bhaskar Jha <35396885+bhaskarjha514@users.noreply.github.com>
2020-08-27 02:35:09 +0530

    Create removeWhiteSpace.java

---
+++ interview ques/removeWhiteSpace.java
@@ -0,0 +1,12 @@
+// remove whitespace from String
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        System.out.println(s.replace(" ",""));
+        
+    }
+}
