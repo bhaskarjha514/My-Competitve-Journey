9272f048ba73f8c8996ae2bb31b7bab6aecd35b6
bhaskarjha514 <bhaskarjha514@gmail.com>
2020-08-26 12:52:19 +0530

    Q4 solved

---
+++ interview ques/palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int temp = n;
+        int reversedNo = 0;
+        while(temp>0){
+            int lastDigit = temp%10;
+            reversedNo = reversedNo*10+lastDigit;
+            temp /= 10;
+        }
+        if(reversedNo == n){
+            System.out.println("yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
